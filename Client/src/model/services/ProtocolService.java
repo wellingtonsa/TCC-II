c526f7abb1437f71b1a8679188f6eb12dd5ffabe
Wellington Araújo <wellingtonaraujosoares@gmail.com>
2021-06-27 12:25:53 -0300

    Moved project to the Java folder

--- Client/src/model/services/ProtocolService.java
+++
@@ -1,8 +0,0 @@
-package model.services;
-
-public interface ProtocolService {
-	public boolean init();
-	public boolean connect(String ip, Integer port);
-	public boolean disconnect();
-	public String sendMessage(String message);
-}
--- Server/src/model/entity/Message.java
+++
@@ -1,5 +0,0 @@
-package model.entity;
-
-public class Message {
-
-}
